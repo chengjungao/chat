2fcf65b6eace296554c26609d52420280a310388
chengjungao <1029745766@qq.com>
2016-01-11 17:54:11 +0800

    修改参数

--- chat/src/com/chengjungao/chat/security/control/SysUserController.java
+++ chat/src/com/chengjungao/chat/security/control/SysUserController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.chengjungao.chat.security.entity.sys_user;
 import com.chengjungao.chat.security.service.SysUserService;
+import com.chengjungao.chat.util.MD5Encrypt;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -30,7 +32,19 @@ public class SysUserController {
 		this.userService = userService;
 	}	
 	@RequestMapping(value="/login.do",method= RequestMethod.POST)
-	public String login(HttpServletRequest request,Model model){
+	public String login(sys_user user,HttpServletRequest request,Model model){
+    //	sys_user user=new sys_user();
+//		String username=request.getParameter("username");
+//		String password=request.getParameter("password");
+//		user.setUsername(username);
+//		user.setPassword(password);
+		user.setPassword(MD5Encrypt.encrypt(user.getPassword()));
+		userService.queryLogin(user);
+        model.addAttribute("user", user);
+	    return "success";
+	}
+	@RequestMapping(value="/adduser.do",method= RequestMethod.POST)
+	public String adduser(HttpServletRequest request,Model model){
     	sys_user user=new sys_user();
 		String username=request.getParameter("username");
 		String password=request.getParameter("password");
